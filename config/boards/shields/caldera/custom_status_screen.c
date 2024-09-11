da11e4f5a5cc4d599be49d1ae8cbce2cd8ff6ecb
Maxcor27 <marco.pellegrino@tii.ae>
2024-09-11 17:52:41 +0400

    fxsw

--- config/boards/shields/caldera/custom_status_screen.c
+++ config/boards/shields/caldera/custom_status_screen.c
@@ -12,7 +12,7 @@ lv_obj_t *zmk_display_status_screen() {
 
     hello_world_label = lv_label_create(screen, NULL);
     lv_label_set_text(hello_world_label, "Hello, Caldera!");
-    lv_obj_align(hello_world_label, NULL, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(hello_world_label                 , LV_ALIGN_CENTER, 0, 0);
 
     return screen;
 }
